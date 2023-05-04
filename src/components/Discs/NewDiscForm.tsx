cbec05f5cea2b6a1a07b99cf0baeeacc74cb0643
NicholasCorcoran <39245904+NicholasCorcoran@users.noreply.github.com>
2023-05-04 13:25:51 -0400

    Add discs to database and display them
    Added functionality to ass discs to bag and display them

--- src/components/Discs/NewDiscForm.tsx
+++ src/components/Discs/NewDiscForm.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
 import classes from './NewDiscForm.module.css'
+import { chronoFirebase } from '../../http';
+import { UserContext } from '../../context/user-context';
+
+const SaveDisc = (uid:string,data: {brand:string, name:string, plastic: string, speed: number, glide: number, turn: number, fade: number}) => {
+    chronoFirebase
+    .post(`/PlayerData/${uid}/MyDiscs.json`, data)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
 export const NewDiscForm = () => {
+    const [brand, setBrand] = React.useState('')
+    const [name, setName] = React.useState('')
+    const [plastic, setPlastic] = React.useState('')
+    const [speed, setSpeed] = React.useState(0)
+    const [glide, setGlide] = React.useState(0)
+    const [fade, setFade] = React.useState(0)
+    const [turn, setTurn] = React.useState(0)
+
+    const ctx = React.useContext(UserContext)
+
+    const onSubmitDisc = (event: React.FormEvent) => {
+        event.preventDefault();
+        const data = {
+            brand,
+            name,
+            plastic,
+            speed,
+            glide,
+            turn,
+            fade,
+        }
+        SaveDisc(ctx.userInfo.uid, data);
+        setBrand('');
+        setName('');
+        setPlastic('');
+        setSpeed(0);
+        setGlide(0);
+        setFade(0);
+        setTurn(0);
+        
+    }
+
+
+
     return <form className={classes.form}>
         <label htmlFor="brand" >
           Disc Brand
@@ -10,6 +57,8 @@ export const NewDiscForm = () => {
           id="brand"
           type="text"
           name="brand"
+          value={brand}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setBrand(event.target.value)}
         />
         <label htmlFor="discname" >
           Disc Name
@@ -18,6 +67,8 @@ export const NewDiscForm = () => {
           id="discname"
           type="text"
           name="discname"
+          value={name}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setName(event.target.value)}
         />
         <label htmlFor="plastic" >
           Plastic
@@ -26,6 +77,8 @@ export const NewDiscForm = () => {
           id="plastic"
           type="text"
           name="plastic"
+          value={plastic}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setPlastic(event.target.value)}
         />
         <label htmlFor="speed" >
           Disc Speed
@@ -34,6 +87,8 @@ export const NewDiscForm = () => {
           id="speed"
           type="number"
           name="speed"
+          value={speed}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setSpeed(+event.target.value)}
         />
         <label htmlFor="glide" >
           Disc Glide
@@ -42,6 +97,8 @@ export const NewDiscForm = () => {
           id="glide"
           type="number"
           name="glide"
+          value={glide}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setGlide(+event.target.value)}
         />
         <label htmlFor="Turn" >
           Disc Turn
@@ -50,6 +107,8 @@ export const NewDiscForm = () => {
           id="turn"
           type="number"
           name="turn"
+          value={turn}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setTurn(+event.target.value)}
         />
         <label htmlFor="fade" >
           Disc Fade
@@ -58,6 +117,9 @@ export const NewDiscForm = () => {
           id="fade"
           type="number"
           name="fade"
+          value={fade}
+           onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setFade(+event.target.value)}
         />
+        <button onClick={onSubmitDisc}>Add Disc</button>
     </form>
 }
