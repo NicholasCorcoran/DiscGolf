e5d6c08a7fda4ac4b4b6ffefc1f590f79d389a3a
Thomas Corcoran <corcoran.thomas17@gmail.com>
2023-05-04 14:26:01 -0400

    Updated scorecard

--- src/components/ScoreCard/ScoreCard.tsx
+++ src/components/ScoreCard/ScoreCard.tsx
@@ -39,23 +39,36 @@ export const ScoreCard: React.FC<{
           ? data.map((i) => {
               return (
                 <div key={Math.random()} className={classes.card}>
-                  <div>
+                  <div key={Math.random()} className={classes.scorecardHeader}>
                     <h1 key={Math.random()}>{i.courseName}</h1>
                     <h3 key={Math.random()}>{i.layoutName}</h3>
+                    <p key={Math.random()}>{i.date.substring(0, 10)}</p>
                   </div>
                   <div className={classes.innerCard}>
                     {i.scoreCard.map((j) => {
                       return (
                         <div key={Math.random()} className={classes.scores}>
-                          <p key={Math.random()}>{j.pName}</p>
+                            <div key={Math.random()} className={classes.scorecardCollumnNames}>
+                                <div>Tee</div>
+                                <div>Distance</div>
+                                <div>Par</div>
+                                <div><p key={Math.random()}>{j.pName}</p></div>
+                            </div>
                           {j.card.map((k) => {
-                            return <p key={Math.random()}>{k}</p>;
-                          })}
+                            return( 
+                                <div key={Math.random()} className={classes.scorecardCollumnScore}>
+                                    <div key={Math.random()} className={classes.scorecardTee}></div>
+                                    <div key={Math.random()} className={classes.scorecardDistance}></div>
+                                    <div key={Math.random()} className={classes.scorecardPar}></div>
+                                    <div key={Math.random()} className={classes.scorecardPlayerOneScore}>
+                                        <p key={Math.random()}>{k}</p>
+                                    </div>
+                                </div>
+                    )})}
                         </div>
                       );
                     })}
                   </div>
-                  <p key={Math.random()}>{i.date.substring(0, 10)}</p>
                 </div>
               );
             })
